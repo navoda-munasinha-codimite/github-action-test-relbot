9784600ec99ed2dccf0fc0449a1e1dc423819949
navoda-munasinha-codimite <navoda.munasinha@codimite.com>
2025-08-17 21:11:08 +0530

    feat: added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ function App() {
  
   return (
     <>
-      <div>
+      <div className="bg-green-500">
+        <h1 className="text-white text-3xl font-bold p-4">Tailwind Test</h1>
       </div>
     </>
   )
