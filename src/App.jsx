7f6fe9642276be10b4db08da3a7a8462c1e7b27e
navoda-munasinha-codimite <navoda.munasinha@codimite.com>
2025-08-17 21:48:55 +0530

    feat: added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import Hero from './components/Hero'
+
 function App() {
- 
   return (
     <>
-      <div className="bg-green-500">
-        <h1 className="text-white text-3xl font-bold p-4">Tailwind Test</h1>
-      </div>
+      <Hero />
     </>
   )
 }
